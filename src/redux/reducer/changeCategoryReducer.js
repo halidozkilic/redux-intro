d7f7d037e6ab7fc8ee58c99bda7192ab05ed92d0
Halid Ozkilic <halit1as@gmail.com>
2020-10-11 19:25:18 +0300

    firt reducer

---
+++ src/redux/reducer/changeCategoryReducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from "../actions/actionTypes"
+import initialState from "./initialState";
+
+//we can use state={} just be sure giving empty state
+export default function changeCategoryReducer(state=initialState.currentCategory,action){
+    switch (action.type){
+
+        case actionTypes.CHANGE_CATEGORY:
+           return action.payload;
+
+            default:
+               return state;
+    }
+}
